5a5d62037118946583cc626137401b33b7e5fdea
nemo-i <tanton509@outlook.com>
2023-11-20 19:03:11 +0200

    add one topic

--- data_entery/lib/core/router/app_router.dart
+++ data_entery/lib/core/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:data_entery/core/router/routers.dart';
+import 'package:data_entery/views/create_data.dart';
 import 'package:data_entery/views/home_page.dart';
 import 'package:data_entery/views/topics_view.dart';
 import 'package:flutter/material.dart';
